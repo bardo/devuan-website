9f1cc03f7a4ef57347dd8a20fd322633bb06aaa4
Corrado Primier <cp@corradoprimier.it>
2014-11-28 19:43:33 -0300

    Add main template

--- devuanweb/urls.py
+++ devuanweb/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+
 #from django.contrib import admin
 
 urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
+
     #url(r'^admin/', include(admin.site.urls)),
 )
