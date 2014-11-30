e94debd30592bbe842913ebb14c9ddb7ea007e30
Corrado Primier <cp@corradoprimier.it>
2014-11-30 15:48:53 -0300

    Add donate page

--- devuanweb/urls.py
+++ devuanweb/urls.py
@@ -4,7 +4,8 @@ from django.views.generic import TemplateView
 #from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name='homepage'),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), {'name': 'homepage'}, name='homepage'),
+    url(r'^donate$', TemplateView.as_view(template_name='donate.html'), {'name': 'donate'}, name='donate'),
 
     #url(r'^admin/', include(admin.site.urls)),
 )
